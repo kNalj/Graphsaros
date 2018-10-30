5682a3d052964c4e28179815a6833befd9439aaa
Luka Drmić <drmishello69@gmail.com>
2018-10-30 17:45:04 +0100

    Add fit mode to LineTrace window, enable gauss fit in LineTrace window

--- graphs/LineTrace.py
+++ graphs/LineTrace.py
@@ -2,8 +2,9 @@ import pyqtgraph as pg
 import numpy as np
 import sys
 
-from PyQt5.QtWidgets import QAction, QApplication
+from PyQt5.QtWidgets import QAction, QApplication, QToolBar
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 
 from scipy.optimize import curve_fit
 from scipy import array
@@ -34,6 +35,8 @@ class LineTrace(BaseGraph):
         self.fit_plot = pg.PlotItem(pen=(60, 60, 60))
 
         self.modes = {"fit": False}
+
+        self.fit_curves = {}
 
         self.init_ui()
 
@@ -67,12 +70,27 @@ class LineTrace(BaseGraph):
 
         self.toggle_fit_mode = QAction(QIcon("img/fit_curve_icon.png"), "Fit_mode")
         self.tools.addAction(self.toggle_fit_mode)
-
-        self.gauss = QAction(QIcon("img/gaussianIcon.png"), "Gaussian_fit")
-        self.tools.addAction(self.gauss)
+        self.toggle_fit_mode.setCheckable(True)
 
         self.exit_action_Btn = QAction(QIcon("img/closeIcon.png"), "Exit")
         self.tools.addAction(self.exit_action_Btn)
+
+        self.init_fit_toolbar()
+
+    def init_fit_toolbar(self):
+        self.fit_toolbar = QToolBar("Fitting options")
+        self.fit_toolbar.actionTriggered[QAction].connect(self.perform_action)
+
+        self.default_graph = QAction(QIcon("img/noneIcon.png"), "Default_graph")
+        self.default_graph.setCheckable(True)
+        self.default_graph.setChecked(True)
+        self.fit_toolbar.addAction(self.default_graph)
+
+        self.gauss = QAction(QIcon("img/gaussianIcon.png"), "Gaussian_fit")
+        self.gauss.setCheckable(True)
+        self.fit_toolbar.addAction(self.gauss)
+        self.addToolBar(Qt.RightToolBarArea, self.fit_toolbar)
+        self.fit_toolbar.hide()
 
     def define_plot_parameters(self):
         pass
@@ -81,36 +99,66 @@ class LineTrace(BaseGraph):
 
         if self.modes["fit"]:
             self.modes["fit"] = False
+            self.fit_toolbar.hide()
             self.main_subplot.removeItem(self.region_select)
             del self.region_select
             self.fit_plot.hide()
             self.fit_plot.clear()
         else:
             self.modes["fit"] = True
+            self.fit_toolbar.show()
             self.region_select = pg.LinearRegionItem([0, 1])
             self.region_select.sigRegionChanged.connect(self.update_selected_region)
             self.region_select.setZValue(10)
             self.region_select.setRegion([self.x_values[0], self.x_values[-1]])
             self.main_subplot.addItem(self.region_select, ignoreBounds=True)
+            self.set_initial_fit_graph_state()
             self.fit_plot.show()
             # add only selected data to graph
 
-    def gaussian_fit_action(self):
+    def set_initial_fit_graph_state(self):
 
-        x = self.x_values
-        y = self.y_values
-        mean = sum(x*y)/sum(y)  # https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
-        sigma = np.sqrt(sum(y * (x - mean) ** 2) / sum(y))
+        self.fit_plot.clear()
+        plot_item = pg.PlotDataItem(self.x_values, self.y_values)
+        self.fit_plot.addItem(plot_item)
+        self.fit_curves["default"] = plot_item
+
+        # Set button states as they should be
+        self.default_graph.setChecked(True)
+        self.gauss.setChecked(False)
+
+    def gaussian_fit_action(self):
 
         def gauss(x, a, x0, sigma):
             return a * exp(-(x - x0) ** 2 / (2 * sigma ** 2))
 
-        p0 = array([max(y), mean, sigma])
-        popt, pcov = curve_fit(gauss, x, y, p0=p0)
-        plot_item = pg.PlotDataItem(x, gauss(x, *popt))
-        plot_item.setPen(255, 0, 0)
-        self.fit_plot.addItem(plot_item)
-        # has to also be removed
+        if self.gauss.isChecked():
+            x = self.x_values
+            y = self.y_values
+            mean = sum(x*y)/sum(y)  # https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
+            sigma = np.sqrt(sum(y * (x - mean) ** 2) / sum(y))
+
+            p0 = array([max(y), mean, sigma])
+            popt, pcov = curve_fit(gauss, x, y, p0=p0)
+            plot_item = pg.PlotDataItem(x, gauss(x, *popt))
+            plot_item.setPen(255, 0, 0)
+            self.fit_plot.addItem(plot_item)
+            self.fit_curves["gauss"] = plot_item
+            # has to also be removed
+        else:
+            self.fit_plot.removeItem(self.fit_curves["gauss"])
+
+    def default_graph_action(self):
+
+        if self.default_graph.isChecked():
+            x = self.x_values
+            y = self.y_values
+
+            plot_item = pg.PlotDataItem(x, y)
+            self.fit_plot.addItem(plot_item)
+            self.fit_curves["default"] = plot_item
+        else:
+            self.fit_plot.removeItem(self.fit_curves["default"])
 
     def update_selected_region(self):
         min_x, max_x = self.region_select.getRegion()
