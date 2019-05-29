dbaaed7a426b3c32ea5f0e5a56e8354900bb9966
Luka Drmić <drmishello69@gmail.com>
2019-05-29 20:44:47 +0200

    Use Labber API (instead of h5py) to read Labber files.

--- data_handlers/LabberDataBuffer.py
+++ data_handlers/LabberDataBuffer.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
-import helpers
-import os
 import h5py
+from Script import Labber
 
 from data_handlers.DataBuffer import DataBuffer
 
@@ -20,13 +19,13 @@ class LabberData(DataBuffer):
         self.legend = {1: "x", 0: "y"}
         super().__init__(location)
 
-        self.gains = self.get_gains()
+        self.alternate = False
+
+        self.log_file = Labber.LogFile(location)
 
         self.matrix_dimensions = self.calculate_matrix_dimensions()
 
         self.axis_values = self.get_axis_data()
-
-        self.data = {}
 
         self.string_type = "Labber"
 
@@ -39,21 +38,30 @@ class LabberData(DataBuffer):
                         y - number of points on y axis
         """
 
-        matrix_dimensions = []
-        file = h5py.File(self.location)
-        channel_names = [channel[0] for channel in file["Data"]["Channel names"]]
-        steping_params = file["Step config"]
-        set_params = [channel[0] for channel in file["Step list"].value]
-        for stepy_boye in steping_params:
-            if stepy_boye in channel_names and stepy_boye in set_params:
-                for item in steping_params[stepy_boye]:
-                    if item == "Step items":
-                        # [(1, 0, 3.05180438e-05, 0.01, 0.1, 0.055, 0.09, 0.005, 19, 0, 0.)]
-                        # above comment is what this "Step items" looks like
-                        # dimension of axis is third to last in the dataset displayed above
-                        matrix_dimensions.append(steping_params[stepy_boye][item].value[0][-3])
-        file.close()
 
+        self.candidates = []
+        for channel in self.log_file.getStepChannels():
+            if len(channel["values"]) > 1:
+                self.candidates.append(channel)
+
+        if len(self.candidates) == 2:
+            for channel in self.log_file.getStepChannels():
+                data = self.log_file.getData(channel["name"])
+                if self.check_if_alternate_direction(data):
+                    self.alternate = True
+
+            matrix_dimensions = [len(self.candidates[0]["values"]), len(self.candidates[1]["values"])]
+
+            self.number_of_set_parameters = 2
+        elif len(self.candidates) == 1:
+            matrix_dimensions = [len(self.log_file.getData(self.log_file.getLogChannels()[0]["name"]))]
+            self.axis_values = {"x": {"name": self.log_file.getStepChannels()[0]["name"],
+                                      "unit": self.log_file.getStepChannels()[0]["unit"]},
+                                "y": {"name": self.log_file.getLogChannels()[0]["name"],
+                                      "unit": self.log_file.getLogChannels()[0]["unit"]}}
+        else:
+            "I need to create a widget that lets you select the things"
+            pass
         if not matrix_dimensions:
             # this should probably try to do a backup way of calculating dimensions, should be implemented in the
             # parrent class
@@ -67,58 +75,45 @@ class LabberData(DataBuffer):
 
         :return:
         """
-        file = h5py.File(self.location)
-        raw_data = file["Data"]
-        data = raw_data["Data"].value
-        self.textual = np.array2string(data)
-        self.number_of_set_parameters = 0
-        self.number_of_measured_parameters = 0
-        set_params = [channel[0] for channel in file["Step list"].value]
-        measured_params = [channel[0] for channel in file["Log list"].value]
 
-        for channel in raw_data["Channel names"].value:
-            if channel[0] in set_params:
-                self.number_of_set_parameters += 1
-            if channel[0] in measured_params:
-                self.number_of_measured_parameters += 1
+        self.textual = np.array2string(self.log_file.getData(self.log_file.getLogChannels()[0]["name"]))
+
+        self.number_of_measured_parameters = self.log_file.getNumberOfLogs()
+        names = [channel["name"] for channel in self.log_file.getLogChannels()]
 
         if self.get_number_of_dimension() == 3:
-            print("Modeling 3d data . . .")
-            y_axis = pd.unique((np.array([axis_values[0] / self.gains["x"] for axis_values in data])).flatten("C"))
-            x_axis = pd.unique((np.array([axis_values[1] / self.gains["y"] for axis_values in data])).flatten("C"))
-            z_axis = (np.array([axis_values[1] for axis_values in data])).flatten("C")
+            x_axis = self.candidates[0]["values"]
+            y_axis = self.candidates[1]["values"]
         else:
-            print("Modeling 2d data . . .")
-            x_axis = [axis_values[0][0] for axis_values in data]
-            y_axis = [axis_values[1][0] for axis_values in data]
+            x_axis = self.candidates[0]["values"]
+            y_axis = self.candidates[1]["values"]
 
         if self.get_number_of_dimension() == 3:
             print("Fetching matrix values . . .")
             matrices = []
-            start_index = self.number_of_set_parameters
-            end_index = self.number_of_set_parameters + self.number_of_measured_parameters
             x_dimension = self.matrix_dimensions[0]
             y_dimension = self.matrix_dimensions[1]
-            for matrix in range(start_index, end_index):
+            for matrix in range(self.number_of_measured_parameters):
+                data = self.log_file.getData(names[matrix])
                 matrix_data = np.zeros((x_dimension, y_dimension))
-                num_of_elements = np.size(matrix_data)
+                num_of_elements = np.size(data)
                 for i in range(x_dimension):
                     for j in range(y_dimension):
                         if i * y_dimension + j < num_of_elements:
-                            if len(z_axis) > i * y_dimension + j:
-                                matrix_data[i][j] = data[j][2 + matrix - start_index][i] / self.gains["z"][matrix - start_index]
-                                self.progress.emit(float(((matrix - start_index) * x_dimension * y_dimension + i * y_dimension + j) /
-                                                   (self.number_of_measured_parameters * x_dimension * y_dimension)))
+                            if self.alternate and j % 2:
+                                matrix_data[i][j] = data[j][-i-1]
                             else:
-                                # if i want to set the missing data to something other then 0 i should do it here
-                                pass
+                                matrix_data[i][j] = data[j][i]
+                            self.progress.emit(
+                                ((matrix - self.number_of_set_parameters) * x_dimension * y_dimension + i * y_dimension + j) /
+                                (self.number_of_measured_parameters * x_dimension * y_dimension))
+                        else:
+                            pass
                 matrices.append(matrix_data)
 
             self.data = {"x": x_axis, "y": y_axis, "matrix": matrices}
             self.progress.emit(1)
             return {"x": x_axis, "y": y_axis, "matrix": matrices}
-        else:
-            pass
 
         self.data = {"x": x_axis, "y": y_axis}
         self.progress.emit(1)
@@ -129,57 +124,37 @@ class LabberData(DataBuffer):
 
         :return:
         """
-        data_dict = {"x": {"name": "", "unit": ""}, "y": {"name": "", "unit": ""}, "z": {}}
-        file = h5py.File(self.location)
-        channel_names = file["Data"]["Channel names"]
-        channel_data = file["Channels"]
+        if self.get_number_of_dimension() == 3:
+            data_dict = {"x": {"name": self.candidates[0]["name"], "unit": self.candidates[0]["unit"]},
+                         "y": {"name": self.candidates[1]["name"], "unit": self.candidates[1]["unit"]},
+                         "z": {}}
 
-        for index, channel_name_tuple in enumerate(channel_names.value):
-            for channel in channel_data.value:
-                if channel_name_tuple[0] == channel[0]:
-                    if index in self.legend:
-                        data_dict[self.legend[index]]["name"] = channel[0]
-                        data_dict[self.legend[index]]["unit"] = channel[3]
-                    else:
-                        mi = index - len(self.legend)
-                        matrix_data = {"name": channel[0], "unit": channel[3]}
-                        data_dict["z"][mi] = matrix_data
+            data = [(channel["name"], channel["unit"]) for channel in self.log_file.getLogChannels()]
+            for index, channel in enumerate(data):
+                channel_dict = {}
+                channel_dict["name"], channel_dict["unit"] = channel
+                data_dict["z"][index] = channel_dict
+
+        elif self.get_number_of_dimension() == 2:
+            data_dict = {"x": {"name": self.log_file.getStepChannels()[0]["name"],
+                               "unit": self.log_file.getStepChannels()[0]["unit"]},
+                         "y": {"name": self.log_file.getLogChannels()[0]["name"],
+                               "unit": self.log_file.getLogChannels()[0]["unit"]}}
 
         return data_dict
 
-    def get_gains(self):
+    def check_if_alternate_direction(self, data):
         """
+        Check if this log file was created with alternate direction option ticked. Take the passed data set and check if
+        the first and the second row are inverse of one another. If yes, the option was ticked.
 
+        :param data:
         :return:
         """
-        file = h5py.File(self.location)
-        channel_names = file["Data"]["Channel names"]
-        channel_data = file["Channels"]
-        gains = {"x": 1, "y": 1, "z": {}}
 
-        for index, channel_name_tuple in enumerate(channel_names.value):
-            for channel in channel_data.value:
-                if channel_name_tuple[0] == channel[0]:
-                    if index in self.legend:
-                        gains[self.legend[index]] = channel[5]
-                    else:
-                        mi = index - len(self.legend)
-                        gains["z"][mi] = channel[5]
-        return gains
-
-    def apply_gain_to_extracted_data(self):
-        """
-
-        :return:
-        """
-        for axis, data in self.data.items():
-            if axis in ["x", "y"]:
-                self.data[axis] = self.data[axis] / self.gains[axis]
-            else:
-                print(data)
-                for index, matrix in enumerate(data):
-                    self.data[axis][index] = self.data[axis][index] / self.gains["z"][index]
-
+        if (data[0] == np.flip(data[1], axis=0)).all():
+            return True
+        return False
 
 def main():
     file_location = "K:\\Measurement\\Andrea\\Majo3\\1stCooldown\\2019\\02\\Data_0217\\I_sweepSG_BG+3000mV.hdf5"
