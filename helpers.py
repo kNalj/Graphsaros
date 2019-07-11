7d22f63f7cfb14e85a01938b45cc2980540f1198
Luka Drmić <drmishello69@gmail.com>
2019-07-11 14:04:35 +0200

    Add helper function for editing axes in LineTrace window

--- helpers.py
+++ helpers.py
@@ -181,17 +181,124 @@ class EditAxisWidget(QWidget):
 
 
 class Edit2DAxisWidget(EditAxisWidget):
-    def __init__(self):
+    def __init__(self, window):
         super(Edit2DAxisWidget, self).__init__()
 
+        self.window = window
+        self.elements = {}
+        self.validators = {}
+
+        self.init_ui()
+
     def init_ui(self):
-        pass
+        _, _, width, height = QDesktopWidget().screenGeometry().getCoords()
+        self.setGeometry(int(0.2 * width), int(0.2 * height), 300, 200)
+
+        self.setWindowTitle("Edit axis data")
+        self.setWindowIcon(QIcon("img/dataStructure.png"))
+
+        layout = QGridLayout()
+        validator = QIntValidator()
+        self.validators["int"] = validator
+
+        for element in ["main_subplot", "fit_plot"]:
+            box = QGroupBox(self)
+            h_layout = QHBoxLayout()
+            plot_label = QLabel(element.capitalize().replace("_", " "), self)
+            layout.addWidget(plot_label)
+            self.elements[element] = {}
+            self.validators[element] = {}
+
+            for side in ["left", "bottom"]:
+                self.elements[element][side] = {}
+                v_layout = QVBoxLayout()
+                axis = self.window.plot_elements[element].getAxis(side)
+
+                plot_axis_text = axis.labelText
+                plot_text = QLineEdit(plot_axis_text)
+                plot_text.setPlaceholderText("Label")
+                self.elements[element][side]["label"] = plot_text
+                plot_current_unit = axis.labelUnits
+                plot_unit = QLineEdit(plot_current_unit)
+                plot_unit.setPlaceholderText("Unit")
+                self.elements[element][side]["unit"] = plot_unit
+                plot_current_font_size = axis.labelStyle["font-size"][:-2]
+                plot_font_size = QLineEdit(plot_current_font_size)
+                plot_font_size.setPlaceholderText("Font size")
+                plot_font_size.setValidator(self.validators["int"])
+                self.elements[element][side]["font_size"] = plot_font_size
+                tick_spacing_major = QLineEdit()
+                tick_spacing_major.setPlaceholderText("Major ticks")
+                self.elements[element][side]["major_ticks"] = tick_spacing_major
+                tick_spacing_minor = QLineEdit()
+                tick_spacing_minor.setPlaceholderText("Minor ticks")
+                self.elements[element][side]["minor_ticks"] = tick_spacing_minor
+                tick_font_size = QLineEdit()
+                tick_font_size.setPlaceholderText("Font")
+                self.elements[element][side]["tick_font"] = tick_font_size
+                self.elements[element][side]["tick_font"].setValidator(self.validators["int"])
+                tick_h_layout = QHBoxLayout()
+                tick_h_layout.addWidget(tick_spacing_major)
+                tick_h_layout.addWidget(tick_spacing_minor)
+                tick_h_layout.addWidget(tick_font_size)
+                v_layout.addWidget(QLabel(side.capitalize()))
+                v_layout.addWidget(plot_text)
+                v_layout.addWidget(plot_unit)
+                v_layout.addWidget(plot_font_size)
+                v_layout.addLayout(tick_h_layout)
+                h_layout.addLayout(v_layout)
+
+            box.setLayout(h_layout)
+            layout.addWidget(box)
+
+        submit_btn = QPushButton("OK", self)
+        submit_btn.clicked.connect(self.data_submitted)
+        layout.addWidget(submit_btn)
+
+        self.setLayout(layout)
 
     def data_submitted(self):
-        pass
+        """
+
+        :return:
+        """
+        data = {"main_subplot": {"left": {}, "bottom": {}, "top": {}},
+                "fit_plot": {"left": {}, "bottom": {}, "top": {}}}
+
+        for element in ["main_subplot", "fit_plot"]:
+            for side in ["left", "bottom"]:
+                data[element][side] = {"name": self.elements[element][side]["label"].text(),
+                                       "unit": self.elements[element][side]["unit"].text(),
+                                       "label_style": {
+                                           'font-size': self.elements[element][side]["font_size"].text() + "pt"},
+                                       "ticks": {"minor": self.elements[element][side]["minor_ticks"].text(),
+                                                 "major": self.elements[element][side]["major_ticks"].text(),
+                                                 "font": self.elements[element][side]["tick_font"].text()}}
+
+        validation_fail = self.validate()
+        if not validation_fail:
+            self.submitted.emit(data)
+        else:
+            self.setFocus(validation_fail)
 
     def validate(self):
-        pass
+        """
+
+        :return:
+        """
+        for element in self.elements:
+            if element in ["main_subplot", "fit_plot"]:
+                for axis in self.elements[element]:
+                    for widget in self.elements[element][axis]:
+                        if self.elements[element][axis][widget].validator() is not None:
+                            validator = self.elements[element][axis][widget].validator()
+                            state = validator.validate(self.elements[element][axis][widget].text(), 0)[0]
+                            if state == QValidator.Acceptable:
+                                return False
+                            elif state == QValidator.Intermediate:
+                                return False
+                            else:
+                                return self.elements[element][axis][widget]
 
 
 class Edit3DAxisWidget(EditAxisWidget):
