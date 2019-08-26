83f090747b947f282a127d8c64b7ef6b8a941452
Luka Drmić <drmishello69@gmail.com>
2019-08-26 16:44:21 +0200

    Make base for settings window

---
+++ widgets/SettingsWidget.py
@@ -0,0 +1,462 @@
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QHBoxLayout, QApplication, QListWidget, QTabWidget, QVBoxLayout, \
+    QScrollArea, QPushButton, QLineEdit, QLabel, QCheckBox, QGroupBox
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtGui import QIcon
+
+import sys
+from graphs.Heatmap import Heatmap
+from data_handlers.QcodesDataBuffer import QcodesData
+from helpers import get_location_basename
+
+
+class SettingsWidget(QWidget):
+    """
+
+    """
+    submitted = pyqtSignal(object)
+
+    def __init__(self, categories):
+        """
+        TODO: Documentation
+
+        """
+        super(QWidget, self).__init__()
+
+        self.categories = ["General", "Display"]
+        if categories is not None:
+            for category in categories:
+                if category not in self.categories:
+                    self.categories.append(category)
+
+        self.option_tabs = {}
+        self.tab_widgets = {}
+
+        self.init_ui()
+
+    def init_ui(self):
+        """
+        TODO: Documentation
+
+        :return:
+        """
+        _, _, width, height = QDesktopWidget().screenGeometry().getCoords()
+        self.setGeometry(int(0.2 * width), int(0.2 * height), 700, 500)
+
+        self.setWindowTitle("Settings")
+        self.setWindowIcon(QIcon("img/settingsIcon.png"))
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        h_layout = QHBoxLayout()
+
+        self.categories_list = QListWidget()
+        for category in self.categories:
+            self.categories_list.addItem(category)
+        h_layout.addWidget(self.categories_list)
+
+        self.options_area = QWidget()
+        options_area_layout = QVBoxLayout()
+        self.options_area.setLayout(options_area_layout)
+        h_layout.addWidget(self.options_area, stretch=1)
+
+        layout.addLayout(h_layout)
+
+        h_layout_buttons = QHBoxLayout()
+
+        apply_btn = QPushButton("Apply")
+        apply_btn.clicked.connect(self.submit_data)
+        h_layout_buttons.addWidget(apply_btn)
+        submit_btn = QPushButton("OK")
+        submit_btn.clicked.connect(self.submit_and_close)
+        h_layout_buttons.addWidget(submit_btn)
+
+        layout.addLayout(h_layout_buttons)
+
+        for category in self.categories:
+            print(category)
+            self.option_tabs[category] = QTabWidget()
+            self.tab_widgets[category] = {}
+            # scroll_area = self.build_tab()
+            # self.add_tab(category, scroll_area, "Dummy " + category + " tab")
+
+        self.categories_list.currentItemChanged.connect(self.update_options_area)
+
+        self.show()
+
+    def build_tab(self):
+        """
+        TODO: Documentation
+
+        :return: QScrollArea that is to be added to the QTabWidget
+        """
+        # Create scroll area
+        scroll_area = QScrollArea(self)
+        scroll_area.setWidgetResizable(True)
+        # Create widget that fills scroll area
+        scroll_area_widget = QWidget(scroll_area)
+        scroll_area.setWidget(scroll_area_widget)
+
+        return scroll_area
+
+    def add_tab(self, category, name, widget):
+        """
+        TODO: Documentation
+
+        :param category:
+        :param widget:
+        :param name:
+        :return: NoneType
+        """
+        self.tab_widgets[category][name] = widget
+        self.option_tabs[category].addTab(widget, name)
+        return
+
+    def fill_tab(self, category, name, layout):
+        """
+        TODO: Documentation
+
+        :param category:
+        :param name:
+        :param layout:
+        :return:
+        """
+        # set the layout as the layout of the tab
+        self.tab_widgets[category][name].setLayout(layout)
+
+    def update_options_area(self, category):
+        """
+
+        :param category:
+        :return:
+        """
+        item = self.options_area.layout().itemAt(0)
+        if item is not None:
+            item.widget().setParent(None)
+        self.options_area.layout().addWidget(self.option_tabs[category.text()])
+
+    def submit_data(self):
+        """
+
+        :return:
+        """
+        values = ["test"]
+        self.submitted.emit(values)
+
+    def submit_and_close(self):
+        """
+
+        :return:
+        """
+        values = ["test"]
+        self.submitted.emit(values)
+        self.close()
+
+    def build_general_tabs(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
+    def build_display_tabs(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
+
+class HeatMapSettings(SettingsWidget):
+    """
+
+    """
+    def __init__(self, window: Heatmap):
+        """
+
+        :param window:
+        """
+        super().__init__([])
+        self.window = window
+
+    def build_general_tabs(self):
+        """
+
+        :return:
+        """
+        buffer_tab = self.build_data_buffer_tab()
+
+        return {"Data buffer": buffer_tab}
+
+    def build_display_tabs(self):
+        """
+
+        :return:
+        """
+        main_graph_tab = self.build_main_graph_settings_tab()
+        line_trace_tab = self.build_line_trace_graph_settings_tab()
+        histogram_tab = self.build_histogram_settings_tab()
+        matlab_bar_tab = self.build_matlab_bar_settings_tab()
+
+        return {"Main graph": main_graph_tab, "Line trace graph": line_trace_tab, "Histogram": histogram_tab,
+                "Matlab bar": matlab_bar_tab}
+
+    def build_data_buffer_tab(self):
+        """
+
+        :return:
+        """
+        layout = QVBoxLayout()
+
+        location_layout = QHBoxLayout()
+        location_label = QLabel("Location of the file: ")
+        if self.window is not None:
+            location_line_edit = QLineEdit(self.window.data_buffer.location)
+        else:
+            location_line_edit = QLineEdit("test")
+        location_line_edit.setDisabled(True)
+        location_layout.addWidget(location_label)
+        location_layout.addWidget(location_line_edit)
+
+        name_layout = QHBoxLayout()
+        name_label = QLabel("Displayed name: ")
+        if self.window is not None:
+            name_line_edit = QLineEdit(get_location_basename(self.window.data_buffer.location))
+        else:
+            name_line_edit = QLineEdit(get_location_basename("test"))
+        show_name_label = QLabel("Show name: ")
+        show_name_checkbox = QCheckBox()
+        name_layout.addWidget(name_label)
+        name_layout.addWidget(name_line_edit)
+        name_layout.addWidget(show_name_label)
+        name_layout.addWidget(show_name_checkbox)
+
+        dimensions_layout = QHBoxLayout()
+        dimensions_label = QLabel("Dimensions: ")
+        if self.window is not None:
+            dimensions_line_edit = QLineEdit(str(self.window.data_buffer.get_matrix_dimensions()))
+        else:
+            dimensions_line_edit = QLineEdit("[25, 51]")
+        dimensions_line_edit.setDisabled(True)
+        dimensions_layout.addWidget(dimensions_label)
+        dimensions_layout.addWidget(dimensions_line_edit)
+
+        layout.addLayout(location_layout)
+        layout.addLayout(name_layout)
+        layout.addLayout(dimensions_layout)
+
+        return layout
+
+    def build_main_graph_settings_tab(self):
+        """
+
+        :return: QLayout that is to be added to the QTabWidget
+        """
+        layout = QVBoxLayout()
+
+        plot_label = QLabel("Main graph settings: ")
+
+        v_layout = QVBoxLayout()
+        for side in ["Left: ", "Bottom: "]:
+            axis = self.window.plot_elements["main_subplot"].getAxis(side[:-2].lower())
+            print(axis._tickSpacing)
+
+            main_group_box = QGroupBox()
+            main_group_box.setTitle(side)
+
+            h_layout = QHBoxLayout()
+            label_box = QGroupBox()
+            label_box.setTitle(side[:-2] + " axis label: ")
+            label_box_layout = QVBoxLayout()
+            for element in ["Label: ", "Unit: ", "Label font size: "]:
+                element_layout = QHBoxLayout()
+                label = QLabel(element)
+                if element == "Label: ":
+                    text = axis.labelText
+                elif element == "Unit: ":
+                    text = axis.labelUnits
+                else:
+                    text = axis.labelStyle["font-size"][:-2]
+                line_edit = QLineEdit(text)
+                element_layout.addWidget(label)
+                element_layout.addWidget(line_edit)
+                label_box_layout.addLayout(element_layout)
+
+            tick_box = QGroupBox()
+            tick_box.setTitle(side[:-2] + " axis ticks: ")
+            tick_box_layout = QVBoxLayout()
+            for element in ["Major ticks: ", "Minor ticks: ", "Tick font size: "]:
+                element_layout = QHBoxLayout()
+                label = QLabel(element)
+                if element == "Major ticks: ":
+                    text = "Major ticks"
+                elif element == "Minor ticks: ":
+                    text = "Minot ticks"
+                else:
+                    text = str(axis._pen.width())
+                line_edit = QLineEdit(text)
+                element_layout.addWidget(label)
+                element_layout.addWidget(line_edit)
+                tick_box_layout.addLayout(element_layout)
+
+            label_box.setLayout(label_box_layout)
+            tick_box.setLayout(tick_box_layout)
+
+            main_group_box.setLayout(h_layout)
+
+            h_layout.addWidget(label_box, stretch=1)
+            h_layout.addWidget(tick_box, stretch=1)
+
+            v_layout.addWidget(main_group_box)
+
+        # Add everything to the main layout
+        layout.addWidget(plot_label)
+        layout.addLayout(v_layout, stretch=1)
+
+        return layout
+
+    def build_line_trace_graph_settings_tab(self):
+        """
+
+        :return:
+        """
+        layout = QVBoxLayout()
+
+        plot_label = QLabel("Line trace graph: ")
+
+        v_layout = QVBoxLayout()
+        for side in ["Left: ", "Bottom: ", "Top: "]:
+            if side != "Top: ":
+                axis = self.window.plot_elements["line_trace_graph"].getAxis(side[:-2].lower())
+            else:
+                axis = self.window.plot_elements["extra_axis"]
+
+            main_group_box = QGroupBox()
+            main_group_box.setTitle(side)
+
+            h_layout = QHBoxLayout()
+            label_box = QGroupBox()
+            label_box.setTitle(side[:-2] + " axis label: ")
+            label_box_layout = QVBoxLayout()
+
+            for element in ["Label: ", "Unit: ", "Label font size: "]:
+                element_layout = QHBoxLayout()
+                label = QLabel(element)
+                if element == "Label: ":
+                    text = axis.labelText
+                elif element == "Unit: ":
+                    text = axis.labelUnits
+                else:
+                    text = axis.labelStyle["font-size"][:-2]
+                line_edit = QLineEdit(text)
+                element_layout.addWidget(label)
+                element_layout.addWidget(line_edit)
+                label_box_layout.addLayout(element_layout)
+
+            tick_box = QGroupBox()
+            tick_box.setTitle(side[:-2] + " axis ticks: ")
+            tick_box_layout = QVBoxLayout()
+            for element in ["Major ticks: ", "Minor ticks: ", "Tick font size: "]:
+                element_layout = QHBoxLayout()
+                label = QLabel(element)
+                if element == "Major ticks: ":
+                    text = "Major ticks"
+                elif element == "Minor ticks: ":
+                    text = "Minot ticks"
+                else:
+                    text = str(axis._pen.width())
+                line_edit = QLineEdit(text)
+                element_layout.addWidget(label)
+                element_layout.addWidget(line_edit)
+                tick_box_layout.addLayout(element_layout)
+
+            label_box.setLayout(label_box_layout)
+            tick_box.setLayout(tick_box_layout)
+
+            main_group_box.setLayout(h_layout)
+
+            h_layout.addWidget(label_box, stretch=1)
+            h_layout.addWidget(tick_box, stretch=1)
+
+            v_layout.addWidget(main_group_box)
+
+        layout.addWidget(plot_label)
+        layout.addLayout(v_layout, stretch=1)
+
+        return layout
+
+    def build_histogram_settings_tab(self):
+        """
+
+        :return:
+        """
+        layout = QVBoxLayout()
+
+        v_layout = QVBoxLayout()
+        axis = self.window.plot_elements["histogram"].axis
+
+        main_group_box = QGroupBox()
+        main_group_box.setTitle("Histogram: ")
+
+        h_layout = QHBoxLayout()
+        label_box = QGroupBox()
+        label_box.setTitle("Left: ")
+        label_box_layout = QVBoxLayout()
+
+        for element in ["Label: ", "Unit: ", "Label font size: "]:
+            element_layout = QHBoxLayout()
+            label = QLabel(element)
+            if element == "Label: ":
+                text = axis.labelText
+            elif element == "Unit: ":
+                text = axis.labelUnits
+            else:
+                text = axis.labelStyle["font-size"][:-2]
+
+            line_edit = QLineEdit(text)
+            element_layout.addWidget(label)
+            element_layout.addWidget(line_edit)
+            label_box_layout.addLayout(element_layout)
+
+        label_box.setLayout(label_box_layout)
+        main_group_box.setLayout(h_layout)
+
+        h_layout.addWidget(label_box, stretch=1)
+
+        v_layout.addWidget(main_group_box)
+
+        layout.addLayout(v_layout)
+
+        return layout
+
+    def build_matlab_bar_settings_tab(self):
+        """
+
+        :return:
+        """
+        layout = QVBoxLayout()
+
+        return layout
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    buffer = QcodesData("C:\\Users\\ldrmic\\Documents\\GitHub\\Graphsaros\\other\\QCoDeS_Daniel_2d1\\IVVI_PLLT_set_IVVI_Ohmic_set.dat")
+    buffer.prepare_data()
+    hm = Heatmap(buffer)
+    ex = HeatMapSettings(hm)
+
+    for name, tab in ex.build_general_tabs().items():
+        scroll = ex.build_tab()
+        ex.add_tab("General", name, scroll)
+        ex.fill_tab("General", name, tab)
+    for name, tab in ex.build_display_tabs().items():
+        scroll = ex.build_tab()
+        ex.add_tab("Display", name, scroll)
+        ex.fill_tab("Display", name, tab)
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
