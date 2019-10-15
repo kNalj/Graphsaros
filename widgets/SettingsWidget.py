51f4b5c295ffdef7694cb0facd36910e9d91d1f4
Luka Drmić <drmishello69@gmail.com>
2019-10-15 17:28:04 +0200

    Extend heatmap windows settings widget options

--- widgets/SettingsWidget.py
+++ widgets/SettingsWidget.py
@@ -40,7 +40,7 @@ class SettingsWidget(QWidget):
         :return:
         """
         _, _, width, height = QDesktopWidget().screenGeometry().getCoords()
-        self.setGeometry(int(0.2 * width), int(0.2 * height), 700, 500)
+        self.setGeometry(int(0.2 * width), int(0.2 * height), 700, 600)
 
         self.setWindowTitle("Settings")
         self.setWindowIcon(QIcon("img/settingsIcon.png"))
@@ -74,11 +74,8 @@ class SettingsWidget(QWidget):
         layout.addLayout(h_layout_buttons)
 
         for category in self.categories:
-            print(category)
             self.option_tabs[category] = QTabWidget()
             self.tab_widgets[category] = {}
-            # scroll_area = self.build_tab()
-            # self.add_tab(category, scroll_area, "Dummy " + category + " tab")
 
         self.categories_list.currentItemChanged.connect(self.update_options_area)
 
@@ -103,7 +100,8 @@ class SettingsWidget(QWidget):
         """
         TODO: Documentation
 
-        :param category:
+        :param category: To which category in the side menu to add this tab to. This category has to already exist in
+        side menu.
         :param widget:
         :param name:
         :return: NoneType
@@ -166,6 +164,13 @@ class SettingsWidget(QWidget):
         """
         raise NotImplementedError
 
+    def build_data_tabs(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
 
 class HeatMapSettings(SettingsWidget):
     """
@@ -176,8 +181,31 @@ class HeatMapSettings(SettingsWidget):
 
         :param window:
         """
-        super().__init__([])
+        # Categories specific for 3d settings widget
+        categories = ["General", "Display", "Data"]
+        super().__init__(categories)
         self.window = window
+        self.extend_ui()
+
+    def extend_ui(self):
+        """
+        Extend the base user interface with stuff required for heatmap settings
+
+        :return:
+        """
+        for name, tab in self.build_general_tabs().items():
+            scroll = self.build_tab()
+            self.add_tab("General", name, scroll)
+            self.fill_tab("General", name, tab)
+        for name, tab in self.build_display_tabs().items():
+            scroll = self.build_tab()
+            self.add_tab("Display", name, scroll)
+            self.fill_tab("Display", name, tab)
+        for name, tab in self.build_data_tabs().items():
+            scroll = self.build_tab()
+            self.add_tab("Data", name, scroll)
+            self.fill_tab("Data", name, tab)
+
 
     def build_general_tabs(self):
         """
@@ -200,6 +228,15 @@ class HeatMapSettings(SettingsWidget):
 
         return {"Main graph": main_graph_tab, "Line trace graph": line_trace_tab, "Histogram": histogram_tab,
                 "Matlab bar": matlab_bar_tab}
+
+    def build_data_tabs(self):
+        """
+
+        :return:
+        """
+        offset_tab = self.build_offset_settings_tab()
+
+        return {"Offset": offset_tab}
 
     def build_data_buffer_tab(self):
         """
@@ -259,7 +296,6 @@ class HeatMapSettings(SettingsWidget):
         v_layout = QVBoxLayout()
         for side in ["Left: ", "Bottom: "]:
             axis = self.window.plot_elements["main_subplot"].getAxis(side[:-2].lower())
-            print(axis._tickSpacing)
 
             main_group_box = QGroupBox()
             main_group_box.setTitle(side)
@@ -436,25 +472,83 @@ class HeatMapSettings(SettingsWidget):
         """
         layout = QVBoxLayout()
 
+        v_layout = QVBoxLayout()
+        axis = self.window.plot_elements["color_bar"].axis
+
+        h_layout = QHBoxLayout()
+        label_box = QGroupBox()
+        label_box.setTitle("Left: ")
+        label_box_layout = QVBoxLayout()
+
+        main_group_box = QGroupBox()
+        main_group_box.setTitle("Matlab bar: ")
+
+        for element in ["Label: ", "Unit: ", "Label font size: "]:
+            element_layout = QHBoxLayout()
+            label = QLabel(element)
+            if element == "Label: ":
+                text = axis.labelText
+            elif element == "Unit: ":
+                text = axis.labelUnits
+            else:
+                if "font-size" in axis.labelStyle:
+                    text = axis.labelStyle["font-size"][:-2]
+
+            line_edit = QLineEdit(text)
+            element_layout.addWidget(label)
+            element_layout.addWidget(line_edit)
+            label_box_layout.addLayout(element_layout)
+
+        label_box.setLayout(label_box_layout)
+        main_group_box.setLayout(h_layout)
+
+        h_layout.addWidget(label_box, stretch=1)
+
+        v_layout.addWidget(main_group_box)
+
+        layout.addLayout(v_layout)
+
+        return layout
+
+    def build_offset_settings_tab(self):
+        """
+
+        :return:
+        """
+        layout = QVBoxLayout()
+
+        settings_label = QLabel("Offset settings")
+
+        v_layout = QVBoxLayout()
+        for orientation in ["Horizontal", "Vertical"]:
+            main_group_box = QGroupBox()
+            main_group_box.setTitle(orientation)
+
+            h_layout = QHBoxLayout()
+            label = QLabel(orientation + " offset: ")
+            line_edit = QLineEdit(str(self.window.offsets[orientation.lower()]))
+
+            h_layout.addWidget(label)
+            h_layout.addWidget(line_edit)
+
+            main_group_box.setLayout(h_layout)
+
+            v_layout.addWidget(main_group_box)
+
+        layout.addWidget(settings_label)
+        layout.addLayout(v_layout, stretch=1)
+
         return layout
 
 
 def main():
     app = QApplication(sys.argv)
 
-    buffer = QcodesData("C:\\Users\\ldrmic\\Documents\\GitHub\\Graphsaros\\other\\QCoDeS_Daniel_2d1\\IVVI_PLLT_set_IVVI_Ohmic_set.dat")
+    buffer = QcodesData("C:\\Users\\ldrmic\\Documents\\GitHub\\Graphsaros\\other\\QCoDeS_2d1\\IVVI_PLLT_set_IVVI_Ohmic_set.dat")
     buffer.prepare_data()
     hm = Heatmap(buffer)
     ex = HeatMapSettings(hm)
 
-    for name, tab in ex.build_general_tabs().items():
-        scroll = ex.build_tab()
-        ex.add_tab("General", name, scroll)
-        ex.fill_tab("General", name, tab)
-    for name, tab in ex.build_display_tabs().items():
-        scroll = ex.build_tab()
-        ex.add_tab("Display", name, scroll)
-        ex.fill_tab("Display", name, tab)
     sys.exit(app.exec_())
 
 
